0e6aec4a88d58eb0f66f55ef9d984a95470a1fd0
Geoff <geoff@geofftech.com>
2025-04-13 20:14:35 +1000

    add pint

--- src/Services/GeolocationService.php
+++ src/Services/GeolocationService.php
@@ -6,25 +6,23 @@ use Stevebauman\Location\Facades\Location;
 
 class GeolocationService
 {
+    public static function get()
+    {
+        $position = Location::get();
 
-  public static function get()
-  {
-    $position = Location::get();
+        if ($position === false && app()->isLocal()) {
+            // return defaults canberra
+            return [-35.353, 149.013];
+        }
 
-    if ($position === false && app()->isLocal()) {
-      // return defaults canberra
-      return [-35.353, 149.013];
+        if ($position === false) {
+            return [0, 0];
+        }
+
+        return [
+            floatval($position->latitude),
+            floatval($position->longitude),
+        ];
+
     }
-
-    if ($position === false) {
-      return [0, 0];
-    }
-
-    return [
-      floatval($position->latitude),
-      floatval($position->longitude),
-    ];
-
-  }
-
 }
--- src/Traits/HasGeolocation.php
+++ src/Traits/HasGeolocation.php
@@ -7,34 +7,31 @@ use Illuminate\Database\Eloquent\Builder;
 
 trait HasGeolocation
 {
+    public function scopeWhereWithinRadius(Builder $query, int $km, float $latitude, float $longitude): void
+    {
 
-  public function scopeWhereWithinRadius(Builder $query, int $km, float $latitude, float $longitude): void
-  {
+        $degrees = $km * 0.0117;
 
-    $degrees = $km * 0.0117;
+        $query
+            ->whereBetween('latitude', [$latitude - $degrees, $latitude + $degrees])
+            ->whereBetween('longitude', [$longitude - $degrees, $longitude + $degrees]);
 
-    $query
-      ->whereBetween('latitude', [$latitude - $degrees, $latitude + $degrees])
-      ->whereBetween('longitude', [$longitude - $degrees, $longitude + $degrees]);
+    }
 
-  }
+    public function scopeWhereWithinRadiusOfMe(Builder $query, int $km): void
+    {
 
-  public function scopeWhereWithinRadiusOfMe(Builder $query, int $km): void
-  {
+        [$latitude, $longitude] = GeolocationService::get();
 
-    [$latitude, $longitude] = GeolocationService::get();
+        $this->scopeWhereWithinRadius($query, $km, $latitude, $longitude);
 
-    $this->scopeWhereWithinRadius($query, $km, $latitude, $longitude);
+    }
 
-  }
-
-  public static function getLatLngAttributes(): array
-  {
-    return [
-      'lat' => 'latitude',
-      'lng' => 'longitude',
-    ];
-  }
-
+    public static function getLatLngAttributes(): array
+    {
+        return [
+            'lat' => 'latitude',
+            'lng' => 'longitude',
+        ];
+    }
 }
-
